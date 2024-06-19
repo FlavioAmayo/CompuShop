2b141d2e91dad675256d14dc9e74a9d5004a50c4
Flavio Amayo <famayo@unitru.edu.pe>
2024-06-19 14:38:36 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function showLogin() {
+    document.getElementById('loginModal').style.display = 'block';
+}
+
+function closeLogin() {
+    document.getElementById('loginModal').style.display = 'none';
+}
+
+function togglePassword() {
+    var passwordField = document.getElementById('password');
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+    } else {
+        passwordField.type = 'password';
+    }
+}
